2aaf35e16fd590a6e1d633f0e5573deb633658e4
J.-Fabian Wenisch <info@jfwenisch.com>
2020-07-19 13:19:54 +0200

    fix: Added entrypoint.sh which sets required values in torrc dependent on the environment variable "mode". Added related deployment yamls for each mode

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine:latest
 LABEL maintainer="info@jfwenisch.com"
 RUN apk update
+# Add required build utitlities
 RUN apk --no-cache add --update \
         gnupg \
         build-base \
@@ -20,13 +21,20 @@ RUN apk --no-cache add --update \
         zstd-dev \
         automake \
         autoconf
+#Add obfs4proxy for usage as bridge
+RUN apk add --no-cache obfs4proxy=0.0.11-r1   --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing
+#Build
 RUN git clone https://git.torproject.org/tor.git
+#Get the latest tag from remote not containing 'alpha' or 'dev' and switch to it (git checkout $release). 
+RUN export release=$(git ls-remote --tags --sort="v:refname" https://git.torproject.org/tor.git | grep -v 'alpha'| grep -v 'dev'| tail -n1| sed  's/.*\///; s/\^{}//');echo $release;cd tor && git checkout $release;
 RUN cd tor && ./autogen.sh
 RUN cd tor && ./configure --disable-asciidoc
 RUN cd tor && make
 RUN cd tor && make install
 RUN rm -rf /var/cache/apk/*
+#Init
 EXPOSE 9050
 COPY torrc /etc/tor/torrc
-ENTRYPOINT [ "tor" ]
-CMD [ "-f", "/etc/tor/torrc" ]
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT [ "/bin/sh" ]
+CMD [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+ #!/bin/sh
+echo "Running jfwenisch/alpine-tor"
+echo -e "Alpine Version: \c" && cat /etc/alpine-release
+echo -e "Tor Version: \c" && tor --version
+echo -e "OBFS4Proxy Version: \c" && obfs4proxy -version
+echo "Searching for environment variable 'mode'"
+if [ "$mode" = "exit" ]
+then
+   echo "Starting up as exit node"
+   echo "ExitRelay 1" >> /etc/tor/torrc
+   echo "DirPort 80" >> /etc/tor/torrc
+   echo "DirPortFrontPage /path/to/html/file" >> /etc/tor/torrc
+elif [ "$mode" = "middle" ]
+then
+   echo "Starting up as middle / guard node"
+   echo "ORPort 443" >> /etc/tor/torrc
+   echo "ExitRelay 0" >> /etc/tor/torrc
+   echo "SocksPort 0" >> /etc/tor/torrc
+   echo "ControlSocket 0" >> /etc/tor/torrc
+elif [ "$mode" = "bridge" ]
+then
+  echo "Starting up as bridge node"
+  echo "ORPort 443" >> /etc/tor/torrc
+  echo "BridgeRelay 1" >> /etc/tor/torrc
+  echo "ServerTransportPlugin obfs4 exec /usr/local/bin/obfs4proxy" >> /etc/tor/torrc
+  echo "ExtORPort auto" >> /etc/tor/torrc
+elif [ "$mode" = "proxy" ]
+then
+  echo "Starting up as proxy"
+  echo "SOCKSPort 0.0.0.0:9050" >> /etc/tor/torrc
+else
+   echo "No mode set. Please refer to the Readme.md on how to run. Exiting."
+   exit;
+fi
+echo "Running tor -f /etc/tor/torrc"
+tor -f /etc/tor/torrc
